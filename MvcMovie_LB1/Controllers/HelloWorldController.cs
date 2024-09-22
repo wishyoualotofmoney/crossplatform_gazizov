91999a33787d46d4ec0538229842e3e5f0735669
Азимут <example.qzagaz@gmail.com>
2024-09-22 15:32:40 +0300

    3 конец

--- MvcMovie_LB1/Controllers/HelloWorldController.cs
+++ MvcMovie_LB1/Controllers/HelloWorldController.cs
@@ -11,17 +11,11 @@ public class HelloWorldController : Controller
     {
         return View();
     }
-    // 
-    //    // GET: /HelloWorld/Welcome/ 
-    //    // Requires using System.Text.Encodings.Web;
-    /*public string Welcome(string name, string surname, int numTimes = 1)
-    {
-        return HtmlEncoder.Default.Encode($"Hello {name}, {surname}, NumTimes is: {numTimes}");
-    }*/
-    //}*/
 
-    public string Welcome(string name, int ID = 1)
+    public IActionResult Welcome(string name, int numTimes = 1)
     {
-        return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        ViewData["Message"] = "Hello " + name;
+        ViewData["NumTimes"] = numTimes;
+        return View();
     }
 }
